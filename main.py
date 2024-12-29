a3a782b99f30f4760aacdb541b1f5367408de467
Matthé Bekkers <matthedeb@hotmail.com>
2024-12-29 18:48:10 -0500

    Added formating to output

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 from functions import *
-import sys
-
-out = sys.stdout
+import pandas as pd
 
 # C:\Users\matth\OneDrive\Code\instagram-flashback\message_1.json
 # C:\Users\matth\OneDrive\Code\instagram-flashback\message_2.json
 json_data = create_json_file()
 
 names, messages = initialize_fields(json_data)
+filtered_messages = remove_self_message_notifications(messages)
+filtered_messages.to_csv("filtered_messages.csv")
 
-messagesdf = pd.DataFrame(messages)
+get_reaction_stats(messages, names)
+get_media_stats(messages, names)
+get_message_stats(messages, names)
 
-out.write(str(names))
-#print("--------------------------------------------------------------------------------")
-#with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-#    out.write(str(messages))
+print("*************************************************")
+for name in names:
+    print("Stats for " + name.get("name") + ":")
+    print("Messages sent: " + str(name.get("message_count")))
+    print("Reaction reacted: " + str(name.get("reaction_count")))
+    print("Photos sent: " + str(name.get("photos_count")))
+    print("Reels and posts sent: " + str(name.get("reels_posts_count")))
+    print("Videos sent: " + str(name.get("video_count")))
+    print("Audios sent " + str(name.get("audio_count")))
+    print(("*************************************************"))
