58a2d83d939a01931bed3017f601b962c8fc3461
Matthé Bekkers <matthedeb@hotmail.com>
2024-12-29 17:36:03 -0500

    completed implementation of reaction and media counting

--- functions.py
+++ functions.py
@@ -2,7 +2,7 @@ import pandas as pd
 import json
 import re
 
-def decode_utf8(str):
+def decode_utf8(str: str) -> str:
     # regex to find utf-8 sequences
     pattern = re.compile(r"(\\u[0-9a-fA-F]{4})+")
 
@@ -56,7 +56,10 @@ def initialize_fields(json_data: list) -> tuple[list[dict], list, list]:
                 "name": name,
                 "message_count": 0,
                 "reaction_count": 0,
-                "media_count": 0
+                "photos_count": 0,
+                "reels_posts_count": 0,
+                "video_count": 0,
+                "audio_count": 0
             }
         )
 
@@ -72,7 +75,7 @@ def initialize_fields(json_data: list) -> tuple[list[dict], list, list]:
 
     return participant_list, message_dataframes
 
-def remove_self_message_notifications(message_list) -> list[dict]:
+def remove_self_message_notifications(message_list: list[list[dict]]) -> list[dict]:
     """
     Since Instagram classifies notifications such as "xxx liked your message" as a sent message, this function removes them since the stats are still counted in the actual messages
 
@@ -82,7 +85,7 @@ def remove_self_message_notifications(message_list) -> list[dict]:
     Returns:
         - A list of filtered messages
     """
-    unwanted_message_pattern = r"[a-zA-Z0-9]+ (changed|reacted|set|liked|sent)"
+    unwanted_message_pattern = r"[a-zA-Z0-9]+ (changed|reacted|set|liked)"
 
     filtered_messages = message_list[~message_list["content"].str.contains(unwanted_message_pattern, na=False)]
     return filtered_messages
@@ -115,6 +118,85 @@ def get_reaction_stats(message_list, user_list):
 
     all_reactions.to_csv("reactions.csv")
     #print(all_reactions)
+
+def get_media_stats(message_list, user_list):
+    """
+    Updates the media_count field for each user
+
+    Args:
+        - A list of messages
+        - A list of users
+
+    Returns:
+        - None
+    """
+
+    #all_photos = message_list["photos"]
+
+    all_photos = pd.DataFrame(columns=["name", "photos"])
+
+    all_posts_reels = pd.DataFrame(columns=["name", "posts_reels"])
+
+    all_videos = pd.DataFrame(columns=["name", "videos"])
+
+    all_audio = pd.DataFrame(columns=["name", "audio"])
+
+    i = 0
+    for row in message_list:
+        all_photos["name"] = message_list["sender_name"]
+        all_photos["photos"] = message_list["photos"]
+
+        all_posts_reels["name"] = message_list["sender_name"]
+        all_posts_reels["posts_reels"] = message_list["share"]
+
+        all_videos["name"] = message_list["sender_name"]
+        all_videos["videos"] = message_list["videos"]
+
+        all_audio["name"] = message_list["sender_name"]
+        all_audio["audio"] = message_list["audio_files"]
+
+    
+    photos_condition = all_photos.iloc[:, 1].isna()
+    all_photos_filtered = all_photos[~photos_condition]
+
+    for index, row in all_photos_filtered.iterrows():
+        sender_name = row[0] 
+        for user in user_list:
+            if user.get("name") == sender_name:
+                user["photos_count"] += 1
+
+    posts_reels_condition = all_posts_reels.iloc[:, 1].isna()
+    all_posts_reels_filtered = all_posts_reels[~posts_reels_condition]
+
+    for index, row in all_posts_reels_filtered.iterrows():
+        sender_name = row[0] 
+        for user in user_list:
+            if user.get("name") == sender_name:
+                user["reels_posts_count"] += 1
+
+    videos_condition = all_videos.iloc[:, 1].isna()
+    all_videos_filtered = all_videos[~videos_condition]
+
+    for index, row in all_videos_filtered.iterrows():
+        sender_name = row[0] 
+        for user in user_list:
+            if user.get("name") == sender_name:
+                user["video_count"] += 1
+
+    audio_condition = all_audio.iloc[:, 1].isna()
+    all_audio_filtered = all_audio[~audio_condition]
+
+    for index, row in all_audio_filtered.iterrows():
+        sender_name = row[0] 
+        for user in user_list:
+            if user.get("name") == sender_name:
+                user["audio_count"] += 1
+    
+    all_photos_filtered.to_csv("photos.csv")
+    all_posts_reels_filtered.to_csv("postsreels.csv")
+    all_videos_filtered.to_csv("videos.csv")
+    all_audio_filtered.to_csv("audio.csv")
+
 
 
 
